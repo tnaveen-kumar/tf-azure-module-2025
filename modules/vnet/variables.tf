5a8fdeddb17dde1d690b513aab9c2b09073d3e82
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2025-09-07 14:21:39 +0530

    Create variables.tf

---
+++ modules/vnet/variables.tf
@@ -0,0 +1,5 @@
+variable "env_name" { type = string }
+variable "rg_name"  { type = string }
+variable "location" { type = string }
+variable "vnet_cidr" { type = string }
+variable "subnet_cidr" { type = string }
