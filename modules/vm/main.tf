994e8718c64f92c7d93f170ee7da9cbfbe5570e7
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2025-09-07 14:18:07 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,12 +1,3 @@
-variable "env_name" { type = string }
-variable "rg_name"  { type = string }
-variable "location" { type = string }
-variable "subnet_id" { type = string }
-variable "public_ip_id" { type = string }
-variable "admin_username" { type = string }
-variable "admin_password" { type = string }
-variable "vm_size" { type = string default = "Standard_B2s" }
-
 resource "azurerm_network_interface" "this" {
   name                = "${var.env_name}-nic"
   location            = var.location
